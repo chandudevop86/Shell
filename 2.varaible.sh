2f5d62285f1529226720ee4b34ba4f9a59d0256c
chandu141186 <chandu141186@gmail.com>
2025-07-05 10:18:00 +0530

    This is heloow world

---
+++ 2.varaible.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ echo "Ramesh: How the day Suresh"
+ echo "Suresh: I am fine How are you Ramesh"
+ echo "Ramesh: I am fine and what are doing Suresh"
+ echo "Suresh: I am learning Devops"
