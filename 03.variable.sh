393cf282d6ea07975db949e486f9606998c72541
chandu141186 <chandu141186@gmail.com>
2025-07-27 15:08:38 +0530

    Shell Script

---
+++ 03.variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1= Ramesh
+PERSON2= Suresh
+
+ echo "$PERSON1: How the day $PERSON2"
+ echo "$PERSON2: I am fine How are you $PERSON1"
+ echo "$PERSON1: I am fine and what are doing $PERSON2"
+ echo "$PERSON2: I am learning Devops"
