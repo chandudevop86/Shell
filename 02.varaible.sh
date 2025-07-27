5c29fdf36487f506b5a8ecf3f96d1d381b819815
chandu141186 <chandu141186@gmail.com>
2025-07-27 15:17:58 +0530

    Shell Script

--- 02.varaible.sh
+++ 02.varaible.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
- echo "Ramesh: How the day Suresh"
- echo "Suresh: I am fine How are you Ramesh"
- echo "Ramesh: I am fine and what are doing Suresh"
- echo "Suresh: I am learning Devops"
+PERSON1=Ramesh
+PERSON2=Suresh
+
+ echo "$PERSON1: How the day $PERSON2"
+ echo "$PERSON2: I am fine How are you $PERSON1"
+ echo "$PERSON1: I am fine and what are doing $PERSON2"
+ echo "$PERSON2: I am learning Devops"
--- 03.variable.sh
+++ 03.variable.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-PERSON1=Ramesh
-PERSON2=Suresh
+DATE=$(date)
 
- echo "$PERSON1: How the day $PERSON2"
- echo "$PERSON2: I am fine How are you $PERSON1"
- echo "$PERSON1: I am fine and what are doing $PERSON2"
- echo "$PERSON2: I am learning Devops"
+echo "Script started executing: ${DATE}"
